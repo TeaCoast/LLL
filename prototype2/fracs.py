I tried fixing precision errors, but nothing worked, so I'm gonna overhaul everything one more time

prototype2/fracs.py
@@ -8,7 +8,7 @@ class Fracs:
     
     def __init__(self, nums: np.ndarray, dens: np.ndarray = None):
         if dens is None:
-            dens = np.ones(nums.shape, dtype=int)
+            dens = np.ones(nums.shape, dtype=object)
         assert nums.shape == dens.shape
         assert 0 not in dens
         self.nums = nums
@@ -16,7 +16,7 @@ class Fracs:
         self.simplify()
         
     def simplify(self):
-        GCDs = np.gcd(self.nums, self.dens)
+        GCDs = np.gcd(self.nums, self.dens, dtype=object)
         self.nums //= GCDs
         self.dens //= GCDs
         self.nums *= np.sign(self.dens)
@@ -40,7 +40,7 @@ class Fracs:
         elif len(fracs1.nums.shape) == len(fracs2.nums.shape) == 2:
             assert fracs1.nums.shape[1] == fracs2.nums.shape[0]
             rows, cols = (fracs1.nums.shape[0], fracs2.nums.shape[1])
-            new_fracs = Fracs(np.zeros((rows, cols), dtype=int))
+            new_fracs = Fracs(np.zeros((rows, cols), dtype=object))
             for col in range(cols):
                 col_fracs = fracs2.T[col]
                 for row in range(cols):
@@ -50,8 +50,8 @@ class Fracs:
 
     def get_row_sums(self) -> 'Fracs':
         assert len(self.shape) == 2
-        dens = np.apply_along_axis(np.lcm.reduce, axis=1, arr=self.dens)
-        nums = np.sum(dens[:, np.newaxis]*self.nums//self.dens, axis=1)
+        dens = np.apply_along_axis(lambda array: np.lcm.reduce(array, dtype=object), axis=1, arr=self.dens)
+        nums = np.sum(dens[:, np.newaxis]*self.nums//self.dens, axis=1, dtype=object)
         return Fracs(nums, dens)
     
     def copy(self) -> 'Fracs':
@@ -73,8 +73,8 @@ class Fracs:
         fracs.simplify()
         return fracs
     
-    def __radd__(fracs1: 'Fracs', fracs2: int | tuple[int, int] | Frac) -> 'Fracs':
-        return fracs1.__add__(fracs2)
+    def __radd__(fracs1: 'Fracs', frac2: int | tuple[int, int] | Frac) -> 'Fracs':
+        return fracs1.__add__(frac2)
 
     def __sub__(fracs1: 'Fracs', fracs2: 'Fracs') -> 'Fracs':
         if isinstance(fracs2, Fracs):
@@ -87,8 +87,8 @@ class Fracs:
         nums = dens*fracs1.nums//fracs1.dens - dens*nums2//dens2
         return Fracs(nums, dens).simplify()
     
-    def __rsub__(fracs1: 'Fracs', fracs2: int | tuple[int, int] | Frac) -> 'Fracs':
-        return fracs1.__neg__().__add__(fracs2)
+    def __rsub__(fracs1: 'Fracs', frac2: int | tuple[int, int] | Frac) -> 'Fracs':
+        return fracs1.__neg__().__add__(frac2)
     
     def inv(self) -> 'Fracs':
         return Fracs(self.dens, self.nums)

prototype2/gso.py
@@ -79,7 +79,7 @@ def compute_GSO_and_coef_fracs(basis: Fracs) -> Fracs:
     size = basis.nums.shape[0]
 
     gso_basis = basis.copy()
-    coefs = Fracs(np.identity(size, dtype=int))
+    coefs = Fracs(np.identity(size, dtype=object))
     for i in range(1, size):
         for j in range(i):
             coefs[i, j] = Fracs.dot(basis[i], gso_basis[j]) / Fracs.dot(gso_basis[j], gso_basis[j])
@@ -104,7 +104,7 @@ def _main():
         [1, 1, 2],
         [1, 2, 3],
         [2, 1, 1]
-    ], dtype=float).T
+    ], dtype=object).T
 
     gso_basis, coefs = compute_GSO_and_coef(basis)
 
@@ -112,7 +112,7 @@ def _main():
     assert np.array_equal(basis.T, np.dot(gso_basis.T, coefs.T))
     assert np.array_equal(basis, np.dot(coefs, gso_basis))
 
-    basis_fracs = Fracs(basis.astype(int))
+    basis_fracs = Fracs(basis)
 
     gso_basis_fracs, coef_fracs = compute_GSO_and_coef_fracs(basis_fracs)
 

prototype2/lll.py
@@ -99,7 +99,7 @@ def compute_LLL(basis: np.ndarray, alpha: float):
     def swap(k):
         assert 0 < k < size
         LLL_basis[[k, k-1]] = LLL_basis[[k-1, k]]
-        #gso.update_GSO_and_coef(LLL_basis, gso_basis, coefs, k-1)
+        new_gso, new_coefs = gso.compute_GSO_and_coef(LLL_basis)
         # optimized GSO update for swap
         v = coefs[k, k-1]
         d = gso_sq[k] + v**2 * gso_sq[k-1]
@@ -133,8 +133,9 @@ def compute_LLL_fracs(basis: Fracs, alpha: float):
     size = basis.nums.shape[0]
 
     gso_basis, coefs = gso.compute_GSO_and_coef_fracs(basis)
+    print(gso_basis.nums.dtype, gso_basis.dens.dtype)
     gso_sq = gso_basis.__pow__(2).get_row_sums()
-    
+
     LLL_basis = basis.copy()
 
     def reduce(k, j):
@@ -151,17 +152,8 @@ def compute_LLL_fracs(basis: Fracs, alpha: float):
     def swap(k):
         assert 0 < k < size
         LLL_basis[k], LLL_basis[k-1] = LLL_basis[k-1], LLL_basis[k].copy()
-        print("start")
-        
-        new_gso_basis, new_coefs = gso.compute_GSO_and_coef_fracs(LLL_basis)
-        new_gso_sq = new_gso_basis.__pow__(2).get_row_sums()
-        print(new_coefs)
-        print(new_gso_basis)
-        print(new_gso_sq)
         v = coefs[k, k-1]
         d = gso_sq[k] + v**2 * gso_sq[k-1]
-        print(v)
-        print(v * gso_sq[k-1] / d)
         coefs[k, k-1] = v * gso_sq[k-1] / d
         gso_sq[k] = gso_sq[k] * gso_sq[k-1] / d
         gso_sq[k-1] = d
@@ -263,27 +255,29 @@ def _main():
         [ 3, -2,  6, -1],
         [ 2, -8, -9, -7],
         [ 8, -9,  6, -4]
-    ], dtype=float)
+    ], dtype=object)
     alpha = 1
     intended_output = np.array([
         [ 2,  3,  1,  1],
         [ 2,  0, -2, -4],
         [-2,  2,  3, -3],
         [ 3, -2,  6, -1]
-    ], dtype=float)
+    ], dtype=object)
 
     lll = LLL(basis, alpha)
     lll.compute()
 
     LLL_basis = compute_LLL(basis, alpha)
-    LLL_basis_fracs = compute_LLL_fracs(Fracs(basis.astype(int)), alpha)
+    frac_basis = Fracs(basis)
+    print(frac_basis.nums.dtype, frac_basis.dens.dtype)
+    LLL_basis_fracs = compute_LLL_fracs(Fracs(basis), alpha)
 
     assert np.array_equal(LLL_basis, np.dot(lll.transform, lll.basis))
 
     print(LLL_basis_fracs)
 
     assert np.array_equal(LLL_basis, intended_output)
-    assert LLL_basis_fracs == Fracs(intended_output.astype(int))
+    assert LLL_basis_fracs == Fracs(intended_output)
 
 if __name__ == "__main__":
     _main()

prototype2/test.py
@@ -1,11 +1,6 @@
 from fracs import Fracs
 import numpy as np
+import math
+from frac import Frac
 
-fracs = Fracs(np.array([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]))
-
-hello = np.array([[1, 2, 3]])
-print(fracs.nums * hello.T)
+print(Frac(43234053184,26785577569) + Frac(18292021504,26785577569) + Frac(2885838400,26785577569) + Frac(959017024,26785577569))